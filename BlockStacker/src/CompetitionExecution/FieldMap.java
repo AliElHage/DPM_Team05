9f0b34b58f130ed95371023f2adcddedddb08c67
Joey2Wen <hao.wen@mail.mcgill.ca>
2016-11-16 13:07:37 -0500

    update map class with some new methods

--- BlockStacker/src/CompetitionExecution/FieldMap.java
+++ BlockStacker/src/CompetitionExecution/FieldMap.java
@@ -56,7 +56,12 @@ public class FieldMap {
 	}
 	
 	
-	
+	/**
+	 * helper method to get the grid object by the coordinate of odometer readings
+	 * @param pointX
+	 * @param pointY
+	 * @return Grid
+	 */
 	private Grid getGrid(double pointX, double pointY){
 		int[] targetGrid = convertPointToGrid(pointX, pointY);
 		
@@ -68,6 +73,12 @@ public class FieldMap {
 		return null;
 	}
 	
+	/**
+	 * helper method to get the grid object by the grid index
+	 * @param gridX
+	 * @param gridY
+	 * @return Grid
+	 */
 	private Grid getGrid(int gridX, int gridY){
 		int[] targetGrid = {gridX, gridY};
 		
@@ -81,15 +92,28 @@ public class FieldMap {
 	
 	
 	/**
-	 * This method will convert the coordinates in odometer to the grid index in the fieldMap.
+	 * This method will convert the coordinates in odometer reading to the grid index in the fieldMap.
 	 * @param pointX
 	 * @param pointY
-	 * @return
+	 * @return int[] 
 	 */
-	private int[] convertPointToGrid(double pointX, double pointY){
+	public static int[] convertPointToGrid(double pointX, double pointY){
 		int gridX = (int)(pointX%GRID_LENGTH);
 		int gridY = (int)(pointY%GRID_LENGTH);
 		return new int[] {gridX, gridY};
+	}
+	
+	
+	/**
+	 * This method will convert grid index in the fieldMap to the the coordinates of the point centered at that grid.
+	 * @param gridX
+	 * @param gridY
+	 * @return double[] 
+	 */
+	public static double[] convertGridToPoint(int gridX, int gridY){
+		double pointX = GRID_LENGTH/2 + (gridX-1)*GRID_LENGTH; 
+		double pointY = GRID_LENGTH/2 + (gridY-1)*GRID_LENGTH; 
+		return new double[] {pointX,pointY};	
 	}
 	
 }
