4dd2d6d76bee6e923762be304e8e996f1b2fb855
Courtney Wright <courtney.wright@mail.mcgill.ca>
2016-11-17 19:33:35 -0500

    Update Parameter Interpretation

--- BlockStacker/src/CompetitionExecution/ParameterInterpretation.java
+++ BlockStacker/src/CompetitionExecution/ParameterInterpretation.java
@@ -7,7 +7,13 @@ package CompetitionExecution;
  */
 public class ParameterInterpretation {
 	
+<<<<<<< HEAD
+	private static final String SERVER_IP = "192.168.2.25";
+	private static final int TEAM_NUMBER = 5;
+	private static TextLCD LCD = LocalEV3.get().getTextLCD();
+=======
 	private int BTN, BSC, CTN, CSC, LRZx, LRZy, URZx, URZy, LGZx, LGZy, UGZx, UGZy;
+>>>>>>> 5ee0f9128fb05f87a0af7c58b3d080827e743544
 	
 	public ParameterInterpretation() {
 		
