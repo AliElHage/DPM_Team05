35a0a5066d5faca6a38d9678812d73de532bffd0
Joey Wen <hao.wen@mail.mcgill.ca>
2016-11-19 15:53:01 -0500

    Add filter method to USPoller
    This method takes a cut off value that reading to be filtered on, and
    number of values to be discarded before return the reading

--- BlockStacker/src/CompetitionExecution/USPoller.java
+++ BlockStacker/src/CompetitionExecution/USPoller.java
@@ -2,6 +2,7 @@ package CompetitionExecution;
 
 
 
+import lejos.hardware.Sound;
 import lejos.robotics.SampleProvider;
 
 // Assuming that the us.fetchSample, and cont.processUSData
@@ -16,6 +17,8 @@ import lejos.robotics.SampleProvider;
  *
  */
 public class USPoller extends Thread{
+	
+	final static int FILTER_RESET_CONTROL = 10;   //number of values to be read to reset the filter 
 	private SampleProvider us;
 	private float[] usData;
 	private double distance;
@@ -28,7 +31,7 @@ public class USPoller extends Thread{
 	}
 
 //  Sensors now return floats using a uniform protocol.
-//  Need to convert US result to an integer [0,255]
+
 	
 	public void run() {
 		
@@ -39,14 +42,47 @@ public class USPoller extends Thread{
 			if(distance > 255){
 				distance = 255;
 			}
- 
-
-			//try { Thread.sleep(50); } catch(Exception e){}		// Poor man's timed sampling	//original 50
+			try { Thread.sleep(50); } catch(Exception e){}		// Poor man's timed sampling	
 		}
 	}
 	
+	/**
+	 * Get US sensor reading.
+	 * @return US sensor reading
+	 */
 	public double readUSDistance() {	
 		return this.distance;
+	}
+	
+	
+	/**
+	 * This method takes a cut off value that reading to be filtered on, and number of values to be discarded 
+	 * to get the filtered value
+	 * @param threshold
+	 * @param filterOut, numbers of readings to be ignored when threshold is reached
+	 * @return filtered Reading
+	 */
+	public double getFilteredValue(int threshold, int filterOut){
+		int filterControlLow = 0, filterControlHigh = 0; 
+		
+		while(true){
+			if (distance > threshold && filterControlHigh < FILTER_RESET_CONTROL) {
+				// when robot reads values above the threshold, increase the filterControlUP only at first
+				filterControlHigh++;
+			}else if(distance > threshold) {
+				// when robot keep getting the cut off above the threshold, then reset the filter
+				filterControlHigh = 0;
+				filterControlLow = 0;
+			}
+			
+			if (distance <= threshold && filterControlLow < filterOut) {
+				// when robot reads values below the threshold, increase the filterControlLOW only, which means to ignore them at first 
+				filterControlLow++;
+			} else if(distance <= threshold) {
+				// when robot keep getting the cut off value below the threshold, then return the distance 
+				return distance;
+			}
+		}
 	}
 	
 	
