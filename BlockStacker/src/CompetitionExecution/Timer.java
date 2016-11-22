7f230fb593f1d0cc982eee5c28a14dceaa957a4e
Joey Wen <hao.wen@mail.mcgill.ca>
2016-11-22 01:19:20 -0500

    Added Timer class
    Timer class keeps track of time for competition by calling
    startTiming(long limit) and passing time limit in seconds

---
+++ BlockStacker/src/CompetitionExecution/Timer.java
@@ -0,0 +1,32 @@
+package CompetitionExecution;
+
+/**
+ * Timer class keeps track of time for competition 
+ * @author joey
+ *
+ */
+public final class Timer {
+	private static long timeLimit;	//store the time limit in second
+	private static long timeStart;	//store the starting time in milisecond
+	
+	
+	private Timer(){}	//private constructor ensures time consistency
+	
+	/**
+	 * This method takes the time limit in second and starts the timing 
+	 * @param limit 
+	 */
+	public static void startTiming(long limit){
+		timeStart = System.currentTimeMillis();
+		timeLimit = limit;
+	}
+	
+	/**
+	 * This method return the time left
+	 * @return time left in seconds
+	 */
+	public static long timeLeft(){
+		long timeElapsed = System.currentTimeMillis() - timeStart;
+		return timeLimit - timeElapsed/1000;
+	}
+}
