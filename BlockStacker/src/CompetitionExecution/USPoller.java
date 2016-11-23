1608615e8e940fca23cc6249e19c8eab93dfcc77
Joey Wen <hao.wen@mail.mcgill.ca>
2016-11-23 14:41:51 -0500

    Added detecting rising edge in USPoller
    1. Added detecting rising edge method
    2. increase US sampling frequency to 20Hz

--- BlockStacker/src/CompetitionExecution/USPoller.java
+++ BlockStacker/src/CompetitionExecution/USPoller.java
@@ -4,12 +4,13 @@ package CompetitionExecution;
 
 import lejos.hardware.Sound;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 // Assuming that the us.fetchSample, and cont.processUSData
 //  methods operate in about 20mS, and that the thread sleeps for
-//  50 mS at the end of each loop, then one cycle through the loop
-//  is approximately 70 mS.  This corresponds to a sampling rate
-//  of 1/70mS or about 14 Hz.
+//  30 mS at the end of each loop, then one cycle through the loop
+//  is approximately 50 mS.  This corresponds to a sampling rate
+//  of 1/50mS or about 20 Hz.
 
 /**
  * polls US sensor to check distance from objects
@@ -18,7 +19,7 @@ import lejos.robotics.SampleProvider;
  */
 public class USPoller extends Thread{
 	
-	final static int FILTER_RESET_CONTROL = 10;   //number of values to be read to reset the filter 
+	final static int FILTER_RESET_CONTROL = 3;   //number of values to be read to reset the filter 
 	private SampleProvider us;
 	private float[] usData;
 	private double distance;
@@ -37,12 +38,12 @@ public class USPoller extends Thread{
 		
 		while (true) { 
 			//edgeDetected = false;
-			us.fetchSample(usData,0);							// acquire data
+			us.fetchSample(usData,0);					// acquire data
 			distance=(usData[0]*100.0);					// extract from buffer
 			if(distance > 255){
 				distance = 255;
 			}
-			try { Thread.sleep(50); } catch(Exception e){}		// Poor man's timed sampling	
+			try { Thread.sleep(30); } catch(Exception e){}		// Poor man's timed sampling	
 		}
 	}
 	
@@ -56,13 +57,13 @@ public class USPoller extends Thread{
 	
 	
 	/**
-	 * This method takes a cut off value that reading to be filtered on, and number of values to be discarded 
-	 * to get the filtered value
+	 * This method takes a cut off value that readings to be filtered on, and number of values to be discarded 
+	 * to read the falling edge
 	 * @param threshold
 	 * @param filterOut, numbers of readings to be ignored when threshold is reached
-	 * @return filtered Reading
+	 * @return filtered distance Reading for a falling edge
 	 */
-	public double getFilteredValue(int threshold, int filterOut){
+	public double getFallingEdge(int threshold, int filterOut){
 		int filterControlLow = 0, filterControlHigh = 0; 
 		
 		while(true){
@@ -70,7 +71,7 @@ public class USPoller extends Thread{
 				// when robot reads values above the threshold, increase the filterControlUP only at first
 				filterControlHigh++;
 			}else if(distance > threshold) {
-				// when robot keep getting the cut off above the threshold, then reset the filter
+				// when robot keep getting the values above the threshold, then reset the filter
 				filterControlHigh = 0;
 				filterControlLow = 0;
 			}
@@ -82,6 +83,39 @@ public class USPoller extends Thread{
 				// when robot keep getting the cut off value below the threshold, then return the distance 
 				return distance;
 			}
+			Delay.msDelay(50);     // keep checking frequency low to the peace of sampling frequency 20Hz
+		}
+	}
+	
+	
+	/**
+	 * This method takes a cut off value that readings to be filtered on, and number of values to be discarded 
+	 * to read the rising edge
+	 * @param threshold
+	 * @param filterOut, numbers of readings to be ignored when threshold is reached
+	 * @return filtered distance Reading for a rising edge
+	 */
+	public double getRisingEdge(int threshold, int filterOut){
+		int filterControlLow = 0, filterControlHigh = 0; 
+		
+		while(true){
+			if (distance >= threshold && filterControlHigh < filterOut) {
+				// when robot reads values above the threshold, increase the filterControlHigh only at first
+				filterControlHigh++;
+			}else if(distance >= threshold) {
+				// when robot keep getting the values above the threshold, then return the distance
+				return distance;
+			}
+			
+			if (distance < threshold && filterControlLow < FILTER_RESET_CONTROL) {
+				// when robot reads values below the threshold, increase the filterControlLOW only, which means to ignore them at first 
+				filterControlLow++;
+			} else if(distance < threshold) {
+				// when robot keep getting the cut off value below the threshold, then reset the filter
+				filterControlHigh = 0;
+				filterControlLow = 0;
+			}
+			Delay.msDelay(50);     // keep checking frequency low to the peace of sampling frequency 20Hz
 		}
 	}
 	
